93830bfd8737cecda6a92ffe71fa1c6c7a5da757
Tom Monks <monksy@gmail.com>
2019-08-19 13:46:00 -0400

    Implemented random background and improved button styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 import QuoteBox from "./QuoteBox";
-import fetchJsonp from "fetch-jsonp"
-import ReactFCCtest from "react-fcctest"
+import fetchJsonp from "fetch-jsonp";
+import ReactFCCtest from "react-fcctest";
+
+const backgroundImages = [
+  "art-background-blue-370799.jpg",
+  "art-background-blur-220072.jpg",
+  "background-blur-blurred-949587.jpg",
+  "background-blur-bright-220067.jpg",
+  "backlit-chiemsee-dawn-1363876.jpg",
+  "blooming-blur-close-up-459059.jpg",
+  "blue-blur-color-932638.jpg",
+  "blurred-background-colors-daytime-1250260-01.jpg",
+  "hugues-de-buyer-mimeure-lQPEChtLjUo-unsplash.jpg"
+];
 
 class App extends React.Component {
   constructor() {
@@ -10,49 +22,51 @@ class App extends React.Component {
 
     this.state = {
       quote: "The man who makes no mistakes does not usually make anything",
-      author: "E.J. Phelps"
+      author: "E.J. Phelps",
+      backgroundNum: 0
     };
 
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.updateQuote()
+    this.updateQuote();
+    this.setState({
+      backgroundNum: Math.floor(Math.random() * backgroundImages.length)
+    });
   }
 
   updateQuote() {
-    /* Storm Consultancy Tech quotes */
-    /*
-    fetch("http://quotes.stormconsultancy.co.uk/random.json")
-      .then(response => response.json())
-      .then(data => {
-        // console.log(data)
-        this.setState({ quote: data.quote, author: data.author })
-      })
-    */
-
-    /* Forismatic API */
-    /* "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?" */
-    fetchJsonp("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=callback", {
-      jsonpCallback: "jsonp"
-    })
+    fetchJsonp(
+      "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=callback",
+      {
+        jsonpCallback: "jsonp"
+      }
+    )
       .then(response => response.json())
       .then(json => {
-        this.setState({ quote: json.quoteText, author: json.quoteAuthor })
+        this.setState({ quote: json.quoteText, author: json.quoteAuthor });
       })
       .catch(error => {
-        console.log('parsing failed', error)
-      })
+        console.log("parsing failed", error);
+      });
   }
 
   componentDidMount() {
-    this.updateQuote()
+    this.updateQuote();
   }
 
   render() {
-    const imageUrl = './images/art-background-blur-220072.jpg';
-    let styles={}
-    // let styles = { backgroundImage: "url(" + imageUrl + ")" };
+    const imageUrl = "./images/" + backgroundImages[this.state.backgroundNum];
+    console.log(
+      "setting background to: " +
+        imageUrl +
+        " (background num: " +
+        this.state.backgroundNum +
+        ")"
+    );
+    // let styles={}
+    let styles = { backgroundImage: "url(" + imageUrl + ")" };
     // let styles = { backgroundColor: "gray" }
     return (
       <div className="app-container" style={styles}>
