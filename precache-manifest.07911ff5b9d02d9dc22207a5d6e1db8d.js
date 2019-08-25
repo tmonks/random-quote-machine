dd2b4921f6eb4e7770a5edb933166b1431ec5a29
Tom Monks <monksy@gmail.com>
2019-08-25 10:15:35 -0400

    Updates

---
+++ precache-manifest.07911ff5b9d02d9dc22207a5d6e1db8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726735071b96dc878953b5aed428aa86",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "b34832f5c9fc335d6237",
+    "url": "/random-quote-machine/static/css/main.411c1dcb.chunk.css"
+  },
+  {
+    "revision": "7bd7ab5241f8fd4958f7",
+    "url": "/random-quote-machine/static/js/2.b11deb34.chunk.js"
+  },
+  {
+    "revision": "b34832f5c9fc335d6237",
+    "url": "/random-quote-machine/static/js/main.9a24d2a5.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
